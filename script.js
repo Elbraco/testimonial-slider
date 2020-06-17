037babf92317e636507f7c486e505b97f8c01278
Gillian Lyle <gillian.lyle@mcdermottgroup.co.uk>
2020-06-17 13:27:17 +0100

    initial attempt

---
+++ script.js
@@ -0,0 +1,19 @@
+let testimonialIndex = 1;
+showTestmonials(testimonialIndex);
+
+function nextTestimonial(n) {
+    showTestmonials(testimonialIndex += n);
+}
+
+function showTestmonials(n) {
+    
+    let testimonials = document.getElementsByClassName("testimonials");
+
+    if (n > testimonials.length) {testimonialIndex = 1}    
+    if (n < 1) {testimonialIndex = testimonials.length}
+    for (let i = 0; i < testimonials.length; i++) {
+        testimonials[i].classList.remove("hide");  
+    }
+
+    testimonials[testimonialIndex-1].classList.add("hide");  
+}
